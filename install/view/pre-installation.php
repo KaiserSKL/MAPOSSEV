81aaadd7111dbc4032b56bd7e3401c96239778e0
KaiserSKL <47432760+KaiserSKL@users.noreply.github.com>
2026-05-18 12:43:35 -0600

    Add files via upload

---
+++ install/view/pre-installation.php
@@ -0,0 +1,133 @@
+<div class="section">
+  <p>1. Por favor, revise su configuración de <strong>PHP</strong> cumpla con los siguientes requisitos:</p>
+  <hr />
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th width="25%">Ajustes</th>
+          <th width="25%">Actual</th>
+          <th>Requerido</th>
+          <th width="15%" class="text-center">Estatus</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>PHP Version</td>
+          <td>
+            <?php echo $current_php_version; ?>
+          </td>
+          <td>
+            <?php echo $php_version_required; ?> >=</td>
+            <td class="text-center">
+              <?php if ($php_version_success) { ?>
+                <i class="status fa fa-check-circle-o"></i>
+              <?php } else { ?>
+                <i class="status fa fa-times-circle-o"></i>
+              <?php } ?>
+            </td>
+          </tr>
+          <tr>
+            <td>allow_url_fopen</td>
+            <td>
+              <?php if ($allow_url_fopen_success) { ?>
+                On
+              <?php } else { ?>
+                Off
+              <?php } ?>
+            </td>
+            <td>On</td>
+            <td class="text-center">
+              <?php if ($allow_url_fopen_success) { ?>
+                <i class="status fa fa-check-circle-o"></i>
+              <?php } else { ?>
+                <i class="status fa fa-times-circle-o"></i>
+              <?php } ?>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="section">
+    <p>2. Comprueba que las extensiones que se enumeran a continuación estén <strong>instaladas/habilitadas</strong> en tu servidor.:</p>
+    <hr />
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th width="25%">Extensiones</th>
+            <th width="25%">Actual</th>
+            <th>Requerido</th>
+            <th width="15%" class="text-center">Estatus</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($settings['extensions'] as $value) { ?>
+            <tr>
+              <td>
+                <?php echo $value; ?>
+              </td>
+              <td>
+                <?php if (extension_loaded($value)) { ?>
+                  On
+                <?php } else { ?>
+                  Off
+                <?php } ?>
+              </td>
+              <td>On</td>
+              <td class="text-center">
+                <?php if (extension_loaded($value)) { ?>
+                  <i class="status fa fa-check-circle-o"></i>
+                <?php } else { ?>
+                  <i class="status fa fa-times-circle-o"></i>
+                <?php } ?>
+              </td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="section">
+      <p>3. Asegúrese de tener permisos de <strong>lectura y escritura</strong> en los siguientes directorios y archivos:</p>
+    <hr />
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Archivos</th>
+            <th width="15%" class="text-center">Estatus</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($settings['writeable_directories'] as $value) { ?>
+            <tr>
+              <td>
+                <?php echo $value; ?>
+              </td>
+              <td class="text-center">
+                <?php if (is_writable('..' . $value)) { ?>
+                  <i class="status fa fa-check-circle-o"></i>
+                  <?php
+                } else {
+                    $all_requirement_success = false; ?>
+                  <i class="status fa fa-times-circle-o"></i>
+                <?php
+                } ?>
+              </td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="panel-footer">
+    <button <?php if (! $all_requirement_success) {
+        echo 'disabled=disabled';
+    } ?> class="btn btn-info
+      form-next"><i class='fa fa-chevron-right'></i> Siguiente</button>
+    </div>
